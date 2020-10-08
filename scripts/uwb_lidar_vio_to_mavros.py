625e22da2c6f0fe64d85328ce07783f70b1b894a
Jorge <jopequ@utu.fi>
2020-10-08 20:18:32 +0300

    Using (uwb_x, uwb_y, lidar_x, vio_o_x, vio_o_y, vio_o_z, vio_o_w) to feed mavros vision_pose

--- scripts/uwb_lidar_vio_to_mavros.py
+++ scripts/uwb_lidar_vio_to_mavros.py
@@ -3,12 +3,12 @@
 import sys
 import rospy
 
-from math				import atan2
+from math                import atan2
 
-from sensor_msgs.msg 	import Range
+from sensor_msgs.msg     import Range
 
-from geometry_msgs.msg 	import Pose
-from geometry_msgs.msg 	import PoseStamped
+from geometry_msgs.msg     import Pose
+from geometry_msgs.msg     import PoseStamped
 
 from nav_msgs.msg       import Odometry
 
@@ -16,88 +16,91 @@ from tf.transformations import quaternion_from_euler
 
 class UWB_Lidar_Position :
 
-	def __init__(self) :
-		'''
-			Init node, subscribe to position data
-		''' 
-		# Init node
-		rospy.init_node("UWB_and_Lidar_Positioning", anonymous=False)
+    def __init__(self) :
+        '''
+            Init node, subscribe to position data
+        ''' 
+        # Init node
+        rospy.init_node("UWB_and_Lidar_Positioning", anonymous=False)
 
-		
+        
         self.lidarz = 0
-		self.orientation_x = 0
+        self.orientation_x = 0
         self.orientation_y = 0
         self.orientation_z = 0
-		self.orientation_w = 1
+        self.orientation_w = 1
 
-		# Publish data
-		self.local_pos_pub 	 = rospy.Publisher('/mavros/vision_pose/pose', PoseStamped, queue_size=10)
+        # Publish data
+        self.local_pos_pub      = rospy.Publisher('/mavros/vision_pose/pose', PoseStamped, queue_size=10)
 
-		# Subscribe to pose topics
-		self.uwb_subscriber = rospy.Subscriber("/dwm1001/tag/drone/position", Pose, self.uwb_cb)
-		self.lidar_subscriber = rospy.Subscriber("/tfmini_ros_node/TFmini", Range, self.lidar_cb)
+        # Subscribe to pose topics
+        self.uwb_subscriber = rospy.Subscriber("/dwm1001/tag/drone/position", Pose, self.uwb_cb)
+        self.lidar_subscriber = rospy.Subscriber("/tfmini_ros_node/TFmini", Range, self.lidar_cb)
 
-		
+        
 
-	def run(self) :
-		'''
-			Infinite loop with 20 Hz sleep
-		''' 
-		# Set Rospy rate
-		rate = rospy.Rate(20.0)
+    def run(self) :
+        '''
+            Infinite loop with 20 Hz sleep
+        ''' 
+        # Set Rospy rate
+        rate = rospy.Rate(20.0)
 
-		# Infinite loop
-		try:
-			while not rospy.is_shutdown() :
-				rate.sleep()
+        # Infinite loop
+        try:
+            while not rospy.is_shutdown() :
+                rate.sleep()
 
-		except KeyboardInterrupt :
-			 rospy.logerr('Keyboard Interrupt detected!')
+        except KeyboardInterrupt :
+             rospy.logerr('Keyboard Interrupt detected!')
 
-	def uwb_cb(self, pos) :
-		'''
-			Get position from UWB, Lidar and VIO (T265)
-		'''
-		pose = PoseStamped()
-		pose.header.stamp = rospy.Time.now()
-		pose.pose.position.x = pos.position.x
-		pose.pose.position.y = pos.position.y
-		pose.pose.position.z = self.lidarz
+    def uwb_cb(self, pos) :
+        '''
+            Get position from UWB, Lidar and VIO (T265)
+        '''
+        pose = PoseStamped()
+        pose.header.stamp = rospy.Time.now()
+        pose.pose.position.x = pos.position.x
+        pose.pose.position.y = pos.position.y
+        pose.pose.position.z = self.lidarz
 
         pose.pose.orientation.x = self.orientation_x
         pose.pose.orientation.y = self.orientation_y
         pose.pose.orientation.z = self.orientation_z
         pose.pose.orientation.w = self.orientation_w
 
-		self.local_pos_pub.publish(pose)
+        self.local_pos_pub.publish(pose)
 
 
-	def lidar_cb(self, pos) :
-		'''
-			Update ground truth position
-		'''
-		self.lidarz = pos.range
-		
+    def lidar_cb(self, pos) :
+        '''
+            Update ground truth position
+        '''
+        self.lidarz = pos.range
+        
     def vio_cb(self, odom) :
         '''
             Update orientation from VIO
         '''
-        self.
+        self.orientation_x = odom.pose.pose.orientation.x
+        self.orientation_y = odom.pose.pose.orientation.y
+        self.orientation_z = odom.pose.pose.orientation.z
+        self.orientation_w = odom.pose.pose.orientation.w
 
 
 if __name__ == '__main__':
-	'''
-		Run the node
-	'''
-	try:
+    '''
+        Run the node
+    '''
+    try:
 
-		# Create new object
-		testing = UWB_Lidar_Position()
+        # Create new object
+        testing = UWB_Lidar_Position()
 
-		# Loop while reading data
-		testing.run()
+        # Loop while reading data
+        testing.run()
 
-	except rospy.ROSInterruptException:
+    except rospy.ROSInterruptException:
 
-		pass
+        pass
 
