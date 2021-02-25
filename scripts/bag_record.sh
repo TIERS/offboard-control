938f4d43ae17a1df3001cb7b568e3aab852ae1f9
Xianjia Yu <xianjia.yu@utu.fi>
2021-02-25 17:51:53 +0200

    add record topic

--- scripts/bag_record.sh
+++ scripts/bag_record.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rosbag record /uav_small_tag_xy /uav_big_tag_xy /uav/offboard/command_waypoint /dwm1001/tag/dronie/position /uav/landing/distance /uav/landing/completed /dwm1001/tag/ugv/position /uav/mavros/local_position/pose
+rosbag record /uav_small_tag_xy /uav_big_tag_xy /uav/offboard/command_waypoint /dwm1001/tag/dronie/position /uav/mavros/setpoint_position/local /rosout /uav/landing/distance /uav/landing/completed /dwm1001/tag/ugv/position /uav/mavros/local_position/pose
