4a6ae33d0a100ddeba14eccc9ecc558a5accac96
Jorge <jopequ@utu.fi>
2021-05-01 20:36:07 +0300

    Add recording script

---
+++ record_bag.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Start with timedate
+dt=$(date '+%d%m%Y_%H%M%S');
+
+# Dump all params
+rosparam dump ${dt}_landing_trials_params_dump.yaml
+
+# Get some of the landing params
+customuwb=$(rosparam get /uav/UWB_VIO_Landing/using_custom_uwb);
+trials=$(rosparam get /uav/UWB_VIO_Landing/max_landing_trials);
+
+# Record excluding the most high freq topics
+rosbag record -a -x "/uav/mavlink/(.*)|/uav/t265/(.*)|/uav/mavros/imu/(.*)|/tf(.*)" -O "${dt}_landing_${trials}_trials_custom_${customuwb}.bag"
