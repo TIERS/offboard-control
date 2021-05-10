04e7246953e7934f331d8ab7a0c1d38820cef9c0
Jorge <jopequ@utu.fi>
2021-05-10 18:58:37 +0300

    organize scripts folder

--- scripts/bag_record.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rosbag record /uav_small_tag_xy /uav_big_tag_xy /uav/offboard/command_waypoint /dwm1001/tag/dronie/position /uav/mavros/setpoint_position/local /rosout /uav/landing/distance /uav/landing/completed /dwm1001/tag/ugv/position /uav/mavros/local_position/pose /uav_tag_center_xy /uav_tag_dist
